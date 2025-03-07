3830150d58e5ef713a66ede3fae37b05e1a2b652
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-03-07 18:32:45 +0000

    Salvando

--- Atividades_Pontuadas/Atividade_Pontuada_2 .py
+++ Atividades_Pontuadas/Atividade_Pontuada_2 .py
@@ -3,24 +3,24 @@ import os
 # Limpar o terminal.
 os.system("clear")
 
-# Solicita os dados do usuário
+# Solicitar os dados do usuário
 nome = input("Digite seu nome: ")
 sexo = input("Digite seu sexo (M/F): ").strip().upper() 
 estado_civil = input("Digite seu estado civil (Solteira/Casada/Divorciada/Viúva): ").strip().upper()
 
-# Inicializa a variável tempo_casada
+# Começar a variável tempo_casada.
 tempo_casada = None 
 
-# Se a pessoa for do sexo feminino e casada, solicita o tempo de casamento
+# Se a pessoa for do sexo feminino e casada, solicitando o tempo de casamento.
 if sexo == "F" and estado_civil == "CASADA":
      tempo_casada = input("Há quantos anos você está casada? ") 
    
-     # Exibe os dados coletados
+     # Exibindo os dados.
      print("\n--- Dados da Pessoa ---") 
      print(f"Nome: {nome}")
      print(f"Sexo: {sexo}")
      print(f"Estado Civil: {estado_civil}") 
    
-     # Exibe o tempo de casada se aplicável
+     # Exibibindo o tempo de casada se assim for.
      if tempo_casada is not None:
           print(f"Tempo de Casada: {tempo_casada} anos") 
---
+++ Atividades_Pontuadas/Atividade_Pontuada_3 .py
@@ -0,0 +1,16 @@
+import os
+#Limpando o terminal. 
+os.system
+
+# Solicitando os valores inteiros A e B ao usuário.
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+# Verificando se os valores são iguais ou diferentes e realizando o cálculo correspondente.
+if A == B: C = A + B 
+
+# Soma se forem iguais.
+else: C = A * B
+
+# Multiplição se forem diferentes.
+# Exibindo resultado.
+print(f"\nO resultado do cálculo é: {C}")
