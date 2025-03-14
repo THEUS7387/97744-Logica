e735d2dea8455782347a480b95e67993b2b4d7d0
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-03-14 17:57:27 +0000

    Salvando

--- Laços_de_Repetição_For . py/Soma_de_Numeros_Inteiros_na_Repetição .py
+++ Laços_de_Repetição_For . py/Soma_de_Numeros_Inteiros_na_Repetição .py
@@ -3,3 +3,10 @@ import os
 #Linpando Terminal. 
 os.system("cls || clear")
 
+soma = 0
+
+for i in range (1, 6):
+    numero = int(input("Digite um numero: "))
+    # soma = soma + numero
+    soma += numero
+    print(F"Soma: {soma}")
