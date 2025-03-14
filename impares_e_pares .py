501bae554734763dc74ee935a06d2ab0cd39248e
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-03-14 19:28:49 +0000

    Salvando

---
+++ impares_e_pares .py
@@ -0,0 +1,16 @@
+import os 
+
+os.system("cls || clear")
+
+pares = 0 
+impares = 0
+
+for i in ranges range(5):
+    if numero % 2 == 0:
+       pares += 1
+    else:
+        impares += 1
+
+print()
+print(f"Quantidade de pares: {pares}")
+print(f"Quantidade de impares: {impares}")
