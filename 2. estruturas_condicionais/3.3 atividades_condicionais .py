00604217a5eb907397c792f2082d2da053a31b78
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-03-20 17:48:09 +0000

    Salvando

--- 2. estruturas_condicionais/3.3 atividades_condicionais .py
+++ 2. estruturas_condicionais/3.3 atividades_condicionais .py
@@ -2,9 +2,9 @@ import os
 
 os.system("clear")
 
-  nota_1 = float(input("digite 1 nota : "))
-  nota_2 = float(input("digite 2 nota : ")) 
-  nota_3 = float(input("digite 3 nota : "))
+nota_1 = float(input("digite 1 nota : "))
+nota_2 = float(input("digite 2 nota : ")) 
+nota_3 = float(input("digite 3 nota : "))
 
 media = (nota_1 + nota_2 + nota_3) / 3
 
