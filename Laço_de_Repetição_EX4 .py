c638a6bd4a2328169352a443e2802687268d3080
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-03-20 17:55:28 +0000

    Salvando

---
+++ Laço_de_Repetição_EX4 .py
@@ -0,0 +1,19 @@
+import os
+
+os.system("cls || clear")
+QUANTIDADE_NOTAS = 2
+soma = 0
+
+for i in range (QUANTIDADE_NOTAS):
+    while True:
+        nota = float(input(f"Digite a {i+1}ª nota: "))
+
+        if nota < 0 or nota > 10: 
+            print("Nota Invalida, tente novamente.\n")
+        else:
+            soma += nota
+            break
+
+media = soma / QUANTIDADE_NOTAS
+
+print(f"Media: {media}")
