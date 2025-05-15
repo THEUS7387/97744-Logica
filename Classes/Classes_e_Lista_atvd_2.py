1f0380b4b96330d454b9f2c3b34876fc8af0d8eb
THEUS7387 <mateus.f.santos11@ba.estudante.senai.br>
2025-05-15 17:56:21 +0000

    Salvando

--- Classes/Classes_e_Lista_atvd_2.py
+++ Classes/Classes_e_Lista_atvd_2.py
@@ -1,20 +1,64 @@
-import os 
-os.system("cls || clear")
+import os
 from dataclasses import dataclass
+
+os.system("cls || clear")
 
 @dataclass
 class Funcionario:
     nome: str
-    data_admissao: int
-    matricula: int
+    data_admissao: str
+    matricula: str
     endereco: str
-
-
+     
+   
 @dataclass
 class Cliente:
     nome: str
-    data_nascimento: int
+    data_nascimento: str
     endereco: str
+   
+def salvar_funcionarios(lista):
+    nome_arquivo = "dados_funcionarios.csv"
 
-nomeF1 = input("Nome")
+    with open(nome_arquivo, "a") as arquivo_fucionarios:
+        for funcionario in lista:
+            arquivo_fucionarios.write(f"{funcionario.nome}, {funcionario.data_admissao}, {funcionario.matricula}, {funcionario.endereco}\n")
+   
+    print("Dados dos funcionários salvos com sucesso.")
+       
+def salvar_clientes(lista):
+    nome_arquivo = "dados_clientes.csv"
 
+    with open(nome_arquivo, "a") as arquivo_clientes:
+        for cliente in lista:
+            arquivo_clientes.write(f"{cliente.nome}, {cliente.data_nascimento}, {cliente.endereco}\n")
+   
+    print("Dados dos clientes salvos com sucesso.")
+       
+   
+lista_funcionarios = []
+lista_clientes = []
+   
+for i in range(1):
+    print("Digite os dados do funcionário: ")
+    funcionario = Funcionario(
+        nome= input("Nome: "),
+        data_admissao=input("Data de nascimento: "),
+        matricula=input("Matrícula: "),
+        endereco=input("Endereço: ")
+    )
+    lista_funcionarios.append(funcionario)
+    print()
+   
+    print("Digite os dados do cliente: ")
+    cliente = Cliente(
+        nome=input("Nome: "),
+        data_nascimento=input("Data de nascimento: "),
+        endereco=input("Endereço: ")
+    )
+    lista_clientes.append(cliente)
+    print()
+   
+       
+salvar_funcionarios(lista_funcionarios)
+salvar_clientes(lista_clientes)
